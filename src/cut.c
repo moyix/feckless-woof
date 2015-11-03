2f8019b9e15d7ca8f51bdd5281641db0066797ab
Tim Leek <tleek@ll.mit.edu>
2015-11-03 09:52:26 -0500

    Add compile_commands.json and instrument main.

--- src/cut.c
+++ src/cut.c
@@ -1,3 +1,8 @@
+static unsigned int lava_val[1000000]; static int lava_first=1;
+void lava_set(unsigned int bug_num, unsigned int val);
+void lava_set(unsigned int bug_num, unsigned int val) { if (lava_first) {int i; lava_first=0; for (i=0; i<1000000; i++) lava_val[i]=0; }  lava_val[bug_num] = val; }
+unsigned int lava_get(unsigned int bug_num);
+unsigned int lava_get(unsigned int bug_num) { return lava_val[bug_num]; }
 /* cut - remove parts of lines of files
    Copyright (C) 1997-2015 Free Software Foundation, Inc.
    Copyright (C) 1984 David M. Ihnat
--- src/join.c
+++ src/join.c
@@ -1,3 +1,8 @@
+static unsigned int lava_val[1000000]; static int lava_first=1;
+void lava_set(unsigned int bug_num, unsigned int val);
+void lava_set(unsigned int bug_num, unsigned int val) { if (lava_first) {int i; lava_first=0; for (i=0; i<1000000; i++) lava_val[i]=0; }  lava_val[bug_num] = val; }
+unsigned int lava_get(unsigned int bug_num);
+unsigned int lava_get(unsigned int bug_num) { return lava_val[bug_num]; }
 /* join - join lines of two files on a common field
    Copyright (C) 1991-2015 Free Software Foundation, Inc.
 
